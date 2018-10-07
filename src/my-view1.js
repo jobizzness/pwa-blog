2b8c613ce49b302c670824a494c0d7ef15685228
jobizzness <jobizzness@gmail.com>
2018-10-07 22:08:56 +0800

    bug fix

--- src/my-view1.js
+++ src/my-view1.js
@@ -43,6 +43,10 @@ class MyView1 extends PolymerElement {
         .glide__track{
           height: 400px;
         }
+        .glide__slide iron-image{
+          width: 100%;
+          height: 100%;
+        }
         .grid{
           display: grid;
         }
@@ -85,6 +89,49 @@ class MyView1 extends PolymerElement {
           max-width: 450px;
           color: white;
         }
+        .subscribe-inner input{
+          border: #e8e8e8;
+          background-color: transparent;
+          border-bottom: 1px solid;
+          border-radius: 0;
+          color: #ffffffc7;
+          margin-bottom: 50px;
+          border-color: #eee;
+          padding: 1em 16px;
+          width: 100vw;
+          max-width: 468px;
+          outline: none;
+          font-size: 1rem;
+        }
+        .subscribe-inner input::placeholder{
+          color: #eee;
+        }
+        .input-wrapper{
+          display: flex;
+        }
+        .subscribe-btn{
+          height: 52px;
+          background: transparent;
+          outline: 0;
+          border: 0;
+        }
+        .slider-slide--content{
+          position: absolute;
+          bottom: 0;
+          color: white;
+          padding: 16px;
+          max-width: 600px;
+        }
+        .glide__slide h1{
+          font-size: 1.5em;
+          font-weight: 400;
+          line-height: 1;
+        }
+        .glide__slide p{
+          font-size: 1em;
+          font-weight: 400;
+          line-height: 1;
+        }
       </style>
 
       <app-hero class="content">
@@ -92,9 +139,42 @@ class MyView1 extends PolymerElement {
           <div class="glide">
             <div class="glide__track" data-glide-el="track">
               <ul class="glide__slides">
-                <li class="glide__slide">0</li>
-                <li class="glide__slide">1</li>
-                <li class="glide__slide">2</li>
+                <li class="glide__slide">
+                  <iron-image 
+                    sizing="cover" 
+                    preload 
+                    src="https://cdn.app.compendium.com/uploads/user/e7c690e8-6ff9-102a-ac6d-e4aebca50425/a3e7d69b-5a30-44b2-af3c-3f7ef2882cd6/File/8860e101166e082a58fb0ade5c362401/dba_banner_tob.jpg"></iron-image>
+                  <div class="slider-slide--content">
+                    <a href="https://blogs.oracle.com/a-veteran-dba%E2%80%99s-insights-on-oracle%E2%80%99s-new-autonomous-data-warehouse">
+                      <h1>A Veteran DBA’s Insights on Oracle’s New Autonomous Data Warehouse</h1>
+                    </a>
+                    <p>Oracle Autonomous Data Warehouse delivers on its promise of automating much of the manual work that has long gone into running a data warehouse.</p>    		
+                  </div>
+                </li>
+                <li class="glide__slide">
+                  <iron-image 
+                    sizing="cover" 
+                    preload 
+                    src="https://cdn.app.compendium.com/uploads/user/e7c690e8-6ff9-102a-ac6d-e4aebca50425/a3e7d69b-5a30-44b2-af3c-3f7ef2882cd6/File/bd0b35989afdd4961d5ba41cf1292cd3/1600x600cloud_fest_2018_blog.jpg"></iron-image>
+                  <div class="slider-slide--content">
+                    <a href="https://blogs.oracle.com/a-veteran-dba%E2%80%99s-insights-on-oracle%E2%80%99s-new-autonomous-data-warehouse">
+                      <h1>A Veteran DBA’s Insights on Oracle’s New Autonomous Data Warehouse</h1>
+                    </a>
+                    <p>Oracle Autonomous Data Warehouse delivers on its promise of automating much of the manual work that has long gone into running a data warehouse.</p>    		
+                  </div>
+                </li>
+                <li class="glide__slide">
+                  <iron-image 
+                    sizing="cover" 
+                    preload 
+                    src="https://cdn.app.compendium.com/uploads/user/e7c690e8-6ff9-102a-ac6d-e4aebca50425/a3e7d69b-5a30-44b2-af3c-3f7ef2882cd6/File/94cec0b1f82a6d21e75b8ceff56db657/java_developer_banner.jpg"></iron-image>
+                  <div class="slider-slide--content">
+                    <a href="https://blogs.oracle.com/a-veteran-dba%E2%80%99s-insights-on-oracle%E2%80%99s-new-autonomous-data-warehouse">
+                      <h1>A Veteran DBA’s Insights on Oracle’s New Autonomous Data Warehouse</h1>
+                    </a>
+                    <p>Oracle Autonomous Data Warehouse delivers on its promise of automating much of the manual work that has long gone into running a data warehouse.</p>    		
+                  </div>
+                </li>
               </ul>
             </div>
             <div class="glide__arrows" data-glide-el="controls">
@@ -179,6 +259,14 @@ class MyView1 extends PolymerElement {
         <div class="subscribe-inner">
           <h1>Never miss a post!</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut viverra nisl rutrum, pretium lectus nec, sagittis dolor....</p>
+          <form action="#" method="get" target="_blank" novalidate="">
+            <div class="input-wrapper">
+              <input type="email" name="EMAIL" id="#" placeholder="Type your email here" required="">
+                <button class="subscribe-btn" type="submit">
+                  <iron-icon icon="my-icons:"></iron-icon>
+                </button>
+            </div>
+          </form>
         </div>
       </section>
     `;
@@ -208,7 +296,7 @@ class MyView1 extends PolymerElement {
       type: 'carousel',
       startAt: 0,
       perView: 1,
-      autoplay: 2000
+      autoplay: 4000
     }).mount()
   }
 
